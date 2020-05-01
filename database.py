5a4b19fcbd2acd633def9355171a6a534f15159b
memreyagci <meydeveloper@tutanota.com>
2020-05-01 19:48:33 +0200

    Switching to SQLAlchemy toolkit. I haven't changed all the codes yet, nor tested the ones that are changed.

--- database.py
+++ database.py
@@ -1,25 +1,78 @@
 import requests
 import pycountry_convert
 import json
+from sqlalchemy import create_engine, MetaData
 
-def create_tables(curr):
-    curr.execute(
-        """ CREATE TABLE IF NOT EXISTS user (
-        telegram_id int,
-        subscribed_country text
-        ); """
-        )
+def create_engine_and_connection():
+    engine = create_engine('sqlite:///covid19.db', echo = True)
 
-    curr.execute(
-        """ CREATE TABLE IF NOT EXISTS country (
-        country_name text UNIQUE,
-        country_continent text,
-        country_stats json
-        ); """
-        )
+    return engine, engine.connect()
 
-def check_new_country(conn, curr):
+def create_tables(engine):
+    meta = MetaData()
+
+    user = Table(
+        'user', meta,
+        Column('telegram_id', Integer),
+        Column('subscribed_country', String)
+    )
+    country = Table(
+        'country', meta,
+        Column('country_name', String),
+        Column('country_continent', String, unique=True),
+        Column('country_stats', JSON)
+    )
+    
+    meta.create_all(engine)
+
+def get_all_countries(conn):
+    statement = select([country.country_name])
+    all_countries = []
+
+    for i in conn.execute(statement):
+        all_countries.append(i[0])
+
+    return all_countries
+
+def get_users(conn):
+    statement = select(user)
+    users_subscriptions = []
+
+    for i in conn.execute(statement):
+        users_subscriptions.append(list(i))
+
+    return users_subscriptions
+
+def get_user_ids(conn):
+    statement = select([user.telegram_id.distinct()])
+    users_ids = []
+
+    for i in conn.execute(statement):
+        users_ids.append(i[0])
+
+    return users_ids
+
+def get_user_subscriptions(conn, telegram_id):
+    statement = select([user.subscribed_country]).where(user.telegram_id == telegram_id)
+    subscription_list = []
+    
+    for i in conn.execute(statement):
+        subscription_list.append(list(i)[0])
+    
+    return subscription_list
+
+def get_country_by_continent(conn, country_continent):
+    statement = select([country.country_name]).where(country.country_continent == country_continent)
+    country_list = []
+
+    for i in conn.execute(statement):
+        country_list.append(list(i)[0])
+
+    return country_list
+
+def check_new_country(conn):
     countries = get_all_countries(curr)
+
     try:
         countries_api = requests.get("https://corona.lmao.ninja/v2/countries").json()
     except json.decoder.JSONDecodeError: #To prevent crashing of bot.py in case of Error 502 of corona.lmao.ninja
@@ -29,22 +82,12 @@ def check_new_country(conn, curr):
             if countries_api[i]["country"] not in countries:
                 try:
                     continent = pycountry_convert.convert_continent_code_to_continent_name(pycountry_convert.country_alpha2_to_continent_code(countries_api[i]["countryInfo"]["iso2"]))
-                    curr.execute("INSERT OR IGNORE INTO country (country_name, country_continent) VALUES (?,?)", (countries_api[i]["country"], continent,))
-                    conn.commit()
+                    statement = country.insert().values(country_name=countries_api[i]["country"], country_continent=continent)
+                    conn.execute(statement)
                 except:
                     pass
 
         return get_all_countries(curr)
-
-def get_all_countries(curr):
-    curr.execute("SELECT country_name FROM country")
-
-    all_countries = []
-
-    for i in curr.fetchall():
-        all_countries.append(i[0])
-
-    return all_countries
 
 def check_if_updated(conn, curr, country_name):
     curr.execute("""SELECT json(country_stats)
@@ -60,65 +103,27 @@ def check_if_updated(conn, curr, country_name):
         try:
             last_stats = json.loads(curr.fetchone()[0])
         except:
-            curr.execute("UPDATE country SET country_stats = ? WHERE country_name = ?", (json.dumps(new_stats), country_name,))
-            conn.commit()
+            statement = country.update().where(country.country_name == country_name).values(country_stats = json.dumps(new_stats))
+            conn.execute(statement)
         else:
             if last_stats != new_stats:
-                curr.execute("UPDATE country SET country_stats = ? WHERE country_name = ?", (json.dumps(new_stats), country_name,))
-                conn.commit()
+                statement = country.update().where(country.country_name == country_name).values(country_stats = json.dumps(new_stats))
+                conn.execute(statement)
                 return True
             else:
                 return False
 
-def get_users(curr):
-    curr.execute("SELECT * FROM user")
-
-    users_subscriptions = []
-
-    for i in curr.fetchall():
-        users_subscriptions.append(list(i))
-
-    return users_subscriptions
-
-def get_user_ids(curr):
-    curr.execute("SELECT DISTINCT telegram_id FROM user")
-
-    users_ids = []
-
-    for i in curr.fetchall():
-        users_ids.append(i[0])
-
-    return users_ids
-
-def get_user_subscriptions(curr, telegram_id):
-    curr.execute("SELECT subscribed_country FROM user WHERE telegram_id = ?", (telegram_id,))
-
-    subscription_list = []
-    
-    for i in curr.fetchall():
-        subscription_list.append(list(i)[0])
-    
-    return subscription_list
-
-def get_country_by_continent(curr, country_continent):
-    curr.execute("SELECT country_name FROM country WHERE country_continent = ?", (country_continent,))
-
-    country_list = []
-
-    for i in curr.fetchall():
-        country_list.append(list(i)[0])
-
-    return country_list
-
 def save_user_subscription(conn, curr, telegram_id, subscribed_country):
-    curr.execute("INSERT OR IGNORE INTO user (telegram_id, subscribed_country) VALUES (?,?)", (telegram_id, subscribed_country,))
-    conn.commit()
+    statement = user.insert().values(telegram_id=telegram_id, subscribed_country=subscribed_country)
+    conn.execute(statement)
 
-def remove_user_subscription(conn, curr, telegram_id, country_to_unsubscribe):
-    curr.execute("DELETE FROM user WHERE telegram_id=? and subscribed_country=?", (telegram_id, country_to_unsubscribe,))
-    conn.commit()
+def remove_user_subscription(conn, telegram_id, country_to_unsubscribe):
+    statement = user.delete().where(user.telegram_id == telegram_id, user.subscribed_country == country_to_unsubscribe)
+    conn.execute(statement)
 
 def get_nonsubscribed_countries_by_continent(curr, telegram_id, country_continent):
+    statement1 = select([user.subscribed_country]).where(user.telegram_id == telegram_id)
+    statement2 = select([country.country_name]).where(country.country_name.)
     curr.execute("SELECT country_name FROM country WHERE country_name NOT IN (SELECT subscribed_country FROM user WHERE telegram_id = ?) AND country_continent = ?", (telegram_id, country_continent,))
 
     nonsubscribed_countries = []
@@ -128,6 +133,6 @@ def get_nonsubscribed_countries_by_continent(curr, telegram_id, country_continen
 
     return nonsubscribed_countries
 
-def delete_user(conn, curr, telegram_id):
-    curr.execute("DELETE FROM user WHERE telegram_id=?", (telegram_id,))
-    conn.commit()
+def delete_user(conn,, telegram_id):
+    statement = user.delete().where(user.telegram_id == telegram_id)
+    conn.execute(statement)
