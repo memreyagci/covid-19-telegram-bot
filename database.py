2301776c73419d58a37d561d6a5038aba38fa2d8
memreyagci <meydeveloper@tutanota.com>
2021-02-17 11:26:28 +0100

    Initial commit for recoding: database.py is rewritten
    I have decided to rewrite this project, since I generally include it
    in my resume, but the code is too clumsily written. My purpose is to
    remove the unnecessary stuff, and writing it in a way that will comply
    with conventions as much as possible.
    
    I switched from sql alchemy to mysql-connector-python library, since I
    believe using SQL syntax gives me more freedom, it lets the code be able
    to do a job with one (or less) function instead of more. See what I did with
    database.get_all, for instance. Instead of using dedicated functions
    like get_all_countries and get_user_ids, which are basically the same
    code with different column/table names, I use get_all as a sql query
    wrapper which anything can be fetched with via indicating table and column
    names as a parameter.

--- database.py
+++ database.py
@@ -1,84 +1,64 @@
+import json
 import requests
 import pycountry_convert
-import json
-import pymysql
-import database_config as conf
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, JSON, and_
-from sqlalchemy.sql import select, insert, update, delete
+import mysql.connector
+from mysql.connector import Error
+import config
 
 class Database:
     def __init__(self):
-        engine = create_engine('mysql+pymysql://{}:{}@{}/{}'.format(conf.mysql["user"], conf.mysql["passwd"], conf.mysql["host"], conf.mysql["db"]))
-        self.create_tables(engine)
-        self.conn = engine.connect()
+        try:
+            self.conn = mysql.connector.connect(
+                    host=config.mysql["host"],
+                    database=config.mysql["database"],
+                    user=config.mysql["user"],
+                    password=config.mysql["password"])
+            self.conn.autocommit=True
+        except Error as e:
+            print(e)
 
-    def get_connection(self):
-        return self.conn
+        self.cursor = self.conn.cursor(buffered=True)
 
-    def create_tables(self, engine):
-        meta = MetaData()
+        self.create_tables()
 
-        self.user = Table(
-            'user', meta,
-            Column('telegram_id', Integer),
-            Column('subscribed_country', String(32)),
-        )
-        self.country = Table(
-            'country', meta,
-            Column('country_name', String(32), unique=True),
-            Column('country_continent', String(32)),
-            Column('country_stats', JSON),
-        )
-        meta.create_all(engine)
+    def create_tables(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS user (tid int NOT NULL, subscription VARCHAR(255))")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS country " \
+                                "(name VARCHAR(255) NOT NULL UNIQUE, " \
+                                "continent VARCHAR(255) NOT NULL, " \
+                                "stats JSON)")
 
-    def get_all_countries(self, conn):
-        statement = select([self.country.c.country_name])
-        all_countries = []
+    def get_all(self, column, table):
+        all_ = []
+        self.cursor.execute("SELECT {} FROM {}".format(column, table))
 
-        for i in conn.execute(statement).fetchall():
-            all_countries.append(i[0])
+        try:
+            for row in self.cursor.fetchall():
+                all_.append(row[0])
+            return all_
+        except:
+            return []
 
-        return all_countries    
-
-    def get_users(self, conn):
-        statement = select([self.user])
+    def get_users(self):
         users_subscriptions = []
+        self.cursor.execute("SELECT * FROM users")
 
-        for i in conn.execute(statement).fetchall():
-            users_subscriptions.append(list(i))
+        for row in self.cursor.fetchall():
+            users_subscriptions.append(list(row))
 
         return users_subscriptions
 
-    def get_user_ids(self, conn):
-        statement = select([self.user.telegram_id.distinct()])
-        users_ids = []
+    def get_where(self, column, table, where1, where2):
+        list_ = []
+        self.cursor.execute("SELECT {} FROM {} WHERE {} = {}".format(column, table, where1, where2))
 
-        for i in conn.execute(statement).fetchall():
-            users_ids.append(i[0])
+        for row in self.cursor.fetchall():
+            list_.append(list(row)[0])
 
-        return users_ids
+        return list_
 
-    def get_user_subscriptions(self, conn, telegram_id):
-        statement = select([self.user.c.subscribed_country]).where(self.user.c.telegram_id == telegram_id)
-        subscription_list = []
-        
-        for i in conn.execute(statement).fetchall():
-            subscription_list.append(list(i)[0])
-        
-        return subscription_list
-
-    def get_country_by_continent(self, conn, country_continent):
-        statement = select([self.country.country_name]).where(country.country_continent == country_continent)
-        country_list = []
-
-        for i in conn.execute(statement).fetchall():
-            country_list.append(list(i)[0])
-
-        return country_list
-
-    def check_new_country(self, conn):
-        countries = self.get_all_countries(conn)
-
+    def check_new_country(self):
+        countries = self.get_all("name", "country")
         try:
             countries_api = requests.get("https://corona.lmao.ninja/v2/countries").json()
         except json.decoder.JSONDecodeError: #To prevent crashing of bot.py in case of Error 502 of corona.lmao.ninja
@@ -87,64 +67,60 @@ class Database:
             for i in range(len(countries_api)):
                 if countries_api[i]["country"] not in countries:
                     try:
-                        continent = pycountry_convert.convert_continent_code_to_continent_name(pycountry_convert.country_alpha2_to_continent_code(countries_api[i]["countryInfo"]["iso2"]))
-                        statement = self.country.insert().values(country_name=countries_api[i]["country"], country_continent=continent).prefix_with('IGNORE')
-                        conn.execute(statement)
+                        continent = pycountry_convert.convert_continent_code_to_continent_name(
+                                pycountry_convert.country_alpha2_to_continent_code(countries_api[i]["countryInfo"]["iso2"]))
+                        self.cursor.execute("INSERT IGNORE INTO country(name, continent) VALUES (%s, %s)",
+                                            (countries_api[i]["country"], continent))
                     except:
                         pass
 
-            return self.get_all_countries(conn)
+            return self.get_all("name", "country")
 
-    def check_if_updated(self, conn, country_name):
-        statement = select([self.country.c.country_stats]).where(self.country.c.country_name == country_name)
-
+    def check_if_updated(self, country):
         try:
-            req = requests.get("https://corona.lmao.ninja/v2/countries/{}".format(country_name)).json()
-        except:
+            req = requests.get("https://corona.lmao.ninja/v2/countries/{}".format(country)).json()
+        except ConnectionError:
             pass
         else:
-            new_stats = [req["cases"] , req["deaths"], req["recovered"] , req["active"], req["critical"] , req["tests"]]
+            new_stats = [req["cases"] , req["deaths"], req["recovered"] ,
+                        req["active"], req["critical"] , req["tests"]]
 
             try:
-                last_stats = json.loads(conn.execute(statement).fetchone()[0])
+                self.cursor.execute("SELECT stats FROM country WHERE country.name = %s", (country))
+                last_stats = json.loads(self.cursor.fetchone()[0])
             except:
-                statement = self.country.update().where(self.country.c.country_name == country_name).values(country_stats = json.dumps(new_stats))
-                conn.execute(statement)
+                self.cursor.execute("UPDATE country SET stats = %s " \
+                                    "WHERE name = %s", (json.dumps(new_stats), country))
             else:
                 if last_stats != new_stats:
-                    statement = self.country.update().where(self.country.c.country_name == country_name).values(country_stats = json.dumps(new_stats))
-                    conn.execute(statement)
+                    self.cursor.execute("UPDATE country SET stats = %s " \
+                                        "WHERE country.name = %s",
+                                        (json.dumps(new_stats), country))
                     return True
                 else:
                     return False
 
-    def save_user_subscription(self, conn, telegram_id, subscribed_country):
-        statement = self.user.insert().values(telegram_id=telegram_id, subscribed_country=subscribed_country)
-        conn.execute(statement)
+    def save_subscription(self, tid, subscription):
+        self.cursor.execute("INSERT INTO user (tid, subcription)" \
+                            "VALUES (%s, %s)", (tid, subscription))
 
-    def remove_user_subscription(self, conn, telegram_id, country_to_unsubscribe):
-        statement = self.user.delete().where(and_(self.user.c.telegram_id == telegram_id, self.user.c.subscribed_country == country_to_unsubscribe))
-        conn.execute(statement)
+    def remove_subscription(self, tid, subscription):
+        self.cursor.execute("DELETE FROM user WHERE tid = %s AND subscription = %s",
+                            (tid, subscription))
 
-    def get_nonsubscribed_countries_by_continent(self, conn, telegram_id, country_continent):
-        to_exclude = select([self.user.c.subscribed_country]).where(
-                    self.user.c.telegram_id == telegram_id)
+    def get_nonsubscribed_countries_by_continent(self, tid, continent):
+        nonsubscribed = []
 
-        statement2 = select([self.country.c.country_name]).where(
-            and_(
-                self.country.c.country_name.notin_(
-                    to_exclude), 
-                    self.country.c.country_continent == country_continent
-                )
-                    )
+        try:
+            self.cursor.execute("SELECT name FROM country WHERE name NOT IN " \
+                                "(SELECT subscription FROM user WHERE tid = %s) AND " \
+                                "continent = %s", (tid, continent))
+            for row in self.cursor.fetchall():
+                nonsubscribed.append(row[0])
 
-        nonsubscribed_countries = []
+            return nonsubscribed
+        except AttributeError:
+            return self.get_all("name", "country")
 
-        for i in conn.execute(statement2).fetchall():
-            nonsubscribed_countries.append(i[0])
-
-        return nonsubscribed_countries
-
-    def delete_user(self, conn, telegram_id):
-        statement = self.user.delete().where(user.c.telegram_id == telegram_id)
-        conn.execute(statement)
+    def delete_user(self, tid):
+        self.cursor.execute("DELETE FROM user WHERE tid = %s", (tid))
